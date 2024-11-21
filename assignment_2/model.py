59decb690d4af9d80d442b711dbf2ff48f722ae0
LiDePa <depascli@mmc116.informatik.uni-augsburg.de>
2024-11-21 11:11:31 +0100

    intermediate features now working

--- assignment_2/model.py
+++ assignment_2/model.py
@@ -8,10 +8,6 @@ class ResNetSegmentationModel(torch.nn.Module):
         super(ResNetSegmentationModel, self).__init__()
         self._use_intermediate_features = use_intermediate_features
 
-        if use_intermediate_features:
-            self._upsample_times2 = torch.nn.Upsample(scale_factor=2, mode="bilinear")
-            self._upsample_times4 = torch.nn.Upsample(scale_factor=4, mode="bilinear")
-
         # get a pretrained ResNet18 model
         self.backbone = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
 
@@ -40,57 +36,47 @@ class ResNetSegmentationModel(torch.nn.Module):
             torch.nn.Upsample(
                 scale_factor=8,
                 mode="bilinear"))
+
+        # create layers for intermediate feature mapping if flag is set
+        if use_intermediate_features:
+            self._upsample_times2 = torch.nn.Upsample(scale_factor=2, mode="bilinear")
+            self._upsample_times4 = torch.nn.Upsample(scale_factor=4, mode="bilinear")
+            self._classify = torch.nn.Conv2d(
+                self.model_segmentation[0].out_channels +
+                self.model_segmentation[4][1].conv2.out_channels +
+                self.model_segmentation[7][1].conv2.out_channels,
+                num_classes,
+                (1, 1))
+
     # TODO: remove prints
     def forward(self, x):
+
         # use early-layer features for upsampling if flag is set
         if self._use_intermediate_features:
-            print(x.shape)
-
             x = self.model_segmentation[0](x) # Conv2d - has stride=(2,2) -> first downsampling to 1/2 of original
-            print("downsample:",x.shape)
             x = self.model_segmentation[1](x) # BatchNorm2d
-            print(x.shape)
             x = self.model_segmentation[2](x) # ReLU
-            print(x.shape)
-
-            # store most expressive features that have 1/2 of original resolution and detach their gradients
-            x_half = x
-            x_half.detach()
-
+            x_half = x                        # store most expressive features that have 1/2 of original resolution
             x = self.model_segmentation[3](x) # MaxPool2d - has stride=(2,2) -> second downsampling to 1/4 of original
-            print("downsample:",x.shape)
             x = self.model_segmentation[4](x) # resnet layer1
-            print(x.shape)
-
-            # store most expressive features that have 1/4 of original resolution and detach their gradients
-            x_quarter = x
-            x_quarter.detach()
-
+            x_quarter = x                     # store most expressive features that have 1/4 of original resolution
             x = self.model_segmentation[5](x) # resnet layer2 - contains Conv2d with stride=(2,2) -> third downsampling to 1/8 of original
-            print("downsample:",x.shape)
             x = self.model_segmentation[6](x) # resnet layer3
-            print(x.shape)
             x = self.model_segmentation[7](x) # resnet layer4
-            print(x.shape)
 
             # upsample current features and x_quarter features such that they are half the original resolution
             x_times4 = self._upsample_times4(x)
             x_quarter_times2 = self._upsample_times2(x_quarter)
 
-            # concatenate the current features with those from previous layers at half the original resolution
-            x = torch.stack((x_times4, x_quarter_times2, x_half))
+            # concatenate the current feature maps with those from previous layers at half the original resolution
+            x = torch.cat((x_times4, x_quarter_times2, x_half), dim=1)
 
+            # classify the concatenated feature maps in a standard convolution
+            x = self._classify(x)
 
-            """
-            # Conv2d
-            x = self.model_segmentation[8](x)
-            print(x.shape)
+            # perform final upsampling to original resolution
+            x = self._upsample_times2(x)
 
-            # Upsample
-            x = self.model_segmentation[9](x)
-            print(x.shape)
-            breakpoint()
-            """
             return x
 
         # use simple bilinear upscaling otherwise
