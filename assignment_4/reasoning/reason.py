1e571cdc68ef474629c5716b7b77f56beb4dcea3
unknown <linusdepascale@gmail.com>
2024-12-16 23:55:06 +0100

    4.2(b)

--- assignment_4/reasoning/reason.py
+++ assignment_4/reasoning/reason.py
@@ -14,9 +14,12 @@ def get_db_schema(driver: neo4j.Driver, database: str) -> tuple[str, str]:
     # if you have different approach of achieving the goal for this exercise, go ahead!
     node_schema = ""
     relation_schema = ""
+
+
     query = "CALL apoc.meta.schema()"
     with driver.session(database=database) as session:
         result = session.run(query)
+
         # schema_data["value"] will contain a lot of information about relationships and nodes
         schema_data = result.data()[0]
 
@@ -24,7 +27,35 @@ def get_db_schema(driver: neo4j.Driver, database: str) -> tuple[str, str]:
         # you could run an additional cypher query like the following
         res = session.run("MATCH (n:Node) RETURN DISTINCT n.category;")
 
-    raise NotImplementedError()
+    # set to be filled in parsing to prevent duplicate relations
+    unique_relations = set()
+
+    # parse schema_data to build a readable schema of the graph database
+    for label, details in schema_data['value'].items():
+        # ignore relation entries as they don't ontain information on direction or start/target nodes
+        if details['type'] == 'node':
+            # build node_schema
+            node_schema += f"NodeType: {label}, Properties: {", ".join(f"{key}: {value['type']}" for key, value in details['properties'].items())}\n"
+
+            # fill unique_relations from nodes' "relationships" value
+            for rel_type, rel_details in details.get('relationships', {}).items():
+                start_node = label
+                direction = rel_details.get('direction', "unknown")
+                properties = ", ".join(
+                    f"{k}: {v['type']}" for k, v in rel_details.get('properties', {}).items()) or "None"
+
+                # implement direction of relation and add it to unique_relations
+                for target_node in rel_details['labels']:
+                    if direction == 'out':
+                        relation = f"({start_node})-[{rel_type}]->({target_node}), Properties: {properties}"
+                    elif direction == 'in':
+                        relation = f"({target_node})-[{rel_type}]->({start_node}), Properties: {properties}"
+                    else:
+                        continue
+                    unique_relations.add(relation)
+
+            # build relation_schema from unique_relations
+            relation_schema = "\n".join(unique_relations)
 
     return node_schema, relation_schema
 
@@ -62,7 +93,14 @@ def main():
     Put your code for exercise 4.1g) here.
     Either implement a command line interface or run all 4 required prompts here.
     """
-    raise NotImplementedError()
+    URI = "bolt://localhost:7687"
+    USERNAME = "neo4j"
+    PASSWORD = "juhujuhu"
+    driver = neo4j.GraphDatabase.driver(URI, auth=(USERNAME, PASSWORD))
+
+    get_db_schema(driver, database="neo4j")
+
+    driver.close()
 
 
 if __name__ == "__main__":
