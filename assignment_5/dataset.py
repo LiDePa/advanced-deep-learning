applied feedback to assignment_5; invisible keypoints now create empty heatmaps and fixed off-by-one error when flipping keypoints

assignment_5/dataset.py
@@ -357,7 +357,7 @@ class SkijumpDataset(torch.utils.data.Dataset):
         if flip:
             # flip image and keypoints inside label
             img = cv2.flip(img, 1)
-            label[:,0] = width_rotated - label[:,0]
+            label[:,0] = width_rotated - label[:,0] - 1
 
             # permute label to account for mirroring of the depicted person
             permutation_order = [0, 4, 5, 6, 1, 2, 3, 10, 11, 12, 7, 8, 9, 15, 16, 13, 14]

assignment_5/metric.py
@@ -5,14 +5,12 @@ import numpy as np
 
 # the following function was added after submitting assignment_5
 def pck(annotations, predictions, torso_indices, t=0.1):
-    annotations = np.array(annotations).astype(np.float32)  # Shape: (n_images, n_keypoints, 3)
-    predictions = np.array(predictions).astype(np.float32)  # Shape: (n_images, n_keypoints, 3)
-
-    # Extract torso keypoints (left hip and right shoulder)
-    left_hip = annotations[:, torso_indices[0], :2].astype(np.float32)
-    right_shoulder = annotations[:, torso_indices[1], :2].astype(np.float32)
+    annotations = np.array(annotations).astype(np.float32)
+    predictions = np.array(predictions).astype(np.float32)
 
     # calculate torso sizes and threshold distances
+    left_hip = annotations[:, torso_indices[0], :2].astype(np.float32)
+    right_shoulder = annotations[:, torso_indices[1], :2].astype(np.float32)
     d_torso = np.linalg.norm(left_hip - right_shoulder, axis=1)
     d_max = d_torso * t
 
@@ -35,7 +33,7 @@ def pck(annotations, predictions, torso_indices, t=0.1):
     correct_mask = correct_mask & ~false_invisibles
     correct_mask = np.where(nan_mask, np.nan, correct_mask)
 
-    # calculate pck per keypoint and overall pck
+    # calculate pck per keypoint and overall pck while ignoring true invisible keypoints marked as nan
     pck_per_keypoint = np.nanmean(correct_mask, axis=0)
     pck_overall= np.mean(pck_per_keypoint)
 